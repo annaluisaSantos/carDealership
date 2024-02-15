e81ef1f72ba8c598b1211a9c62404b157046e3ea
Anna Santos <analuisasantos.3112@gmail.com>
2024-02-15 11:05:29 +0000

    First commit

---
+++ src/main/java/com/cardealership/CarDealershipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipManagementSystemApplication.class, args);
+	}
+
+}
