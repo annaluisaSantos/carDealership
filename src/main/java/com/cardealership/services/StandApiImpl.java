e5a97d2da83716d21b4597df957c1f3176a6264d
Anna Santos <analuisasantos.3112@gmail.com>
2024-02-15 22:04:56 +0000

    error fixed

--- src/main/java/com/cardealership/services/StandApiImpl.java
+++ src/main/java/com/cardealership/services/StandApiImpl.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-public class StandAPIImpl implements StandAPI {
+public class StandApiImpl implements StandAPI {
 
     @Autowired
     private VehicleBrandsRepository vehicleBrandRepository;
